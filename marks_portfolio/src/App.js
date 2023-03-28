3acc89c92617cd819715a69d2742b35eff89c5a1
MarkGiddings202 <gavingiddings1997@gmail.com>
2023-03-28 15:53:02 -0400

    Portfolio template

--- marks_portfolio/src/App.js
+++ marks_portfolio/src/App.js
@@ -1,6 +1,8 @@
 import NavBar from "./components/Navbar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <NavBar/>
       <Home/>
       <SocialLinks/>
+      <About/>
+      <Portfolio/>
     </div>
   );
 }
--- marks_portfolio/src/components/SocialLinks.jsx
+++ marks_portfolio/src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "/Mark Giddings Resume.pdf", 
       style: "rounded-br-md",
       download: true,
     },
