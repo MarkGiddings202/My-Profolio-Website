27bd7711aa311fd72db8f3d97a9cfb685d9e8e05
MarkGiddings202 <gavingiddings1997@gmail.com>
2023-04-04 14:41:25 -0400

    Fixed typo

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -46,7 +46,7 @@ const Experience = () => {
     {
       id: 6,
       src: tablePlus,
-      title: "Next JS",
+      title: "Table Plus",
       style: "shadow-white",
     },
     {
@@ -101,7 +101,7 @@ const Experience = () => {
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-10 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
